02ff46819c4b757e97f8af4d03d6a024e4ed6ffb
Kostas Minaidis <minaidisk@gmail.com>
2022-08-11 21:52:04 +0300

    Update

--- src/api.js
+++ src/api.js
@@ -4,7 +4,7 @@ const app        = express();
 const router     = express.Router();
 
 router.get("/", (req,res)=>{
-    res.json({ message: "Express deployed to Netlify!" })
+    res.json({ message: "Express deployed to Netlify! v1.1" })
 });
 
 app.use( "/.netlify/functions/api", router );
